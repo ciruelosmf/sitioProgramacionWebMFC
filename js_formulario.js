3098224d5febdf4444166858cb9974c0d2715986
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2022-06-01 21:37:02 +0000

    js de formulario cerca de completo

--- js_formulario.js
+++ js_formulario.js
@@ -1,34 +1,42 @@
  // 19/5/2022
 
         // buscar elementos
-    var checks_comidas = document.getElementById('divChecksComidas2');
+ 
     var add_more_fields = document.getElementById('add_more_fields');
     var remove_fields = document.getElementById('remove_fields');
-    var checks_comidassi = document.getElementById('asd');
-    var controlss = document.getElementById('controls');
-
+    var check_carnesylomos = document.getElementById('asd');
+    var controlss = document.getElementById('controls'); 
+    var divChecksComidas = document.getElementById('divChecksComidas2'); 
     // TODO acá => agregar input de texto si checkbox==checked     2
 
 
 
-    checks_comidassi.onchange = function(){
+    check_carnesylomos.onchange = function(){
+console.log("hola");
+
+      
+      if (check_carnesylomos.checked){
+        
+         console.log("hola2");
           var newField = document.createElement('input');
           newField.setAttribute('type','text');
-          newField.setAttribute('name','vehicle1[]');
-          newField.setAttribute('class','vehicle1');
+          newField.setAttribute('name','vehicl');
+          newField.setAttribute('class','vehicl');
           newField.setAttribute('size',50);
           newField.setAttribute('placeholder','Another Field');
-          checks_comidas.appendChild(newField);
+          divChecksComidas.appendChild(newField);
+        
+      } 
+        
+      else if (!check_carnesylomos.checked){
+        
+while (divChecksComidas.hasChildNodes()) {
+                divChecksComidas.removeChild(divChecksComidas.lastChild);
+            }
+        console.log("!");
+}
 
 
-          var newField2 = document.createElement('a');
-          newField2.setAttribute('href','#');
- 
-          newField2.setAttribute('class','vehicle12');
-          controlss.style.display = "inline"; 
-          newField2.setAttribute('text','asdss');
-          newField2.setAttribute('size',50);
-          checks_comidas.appendChild(newField2);
 
 
          }
@@ -53,10 +61,20 @@
 
              // TODO acá => quitar input de texto si checkbox!=checked
     remove_fields.onclick = function(){
-          var input_tags = survey_options.getElementsByTagName('input');
-          if(input_tags.length > 2) {
-            survey_options.removeChild(input_tags[(input_tags.length) - 1]);
-          }
+      
+      divChecksComidas.removeChild(divChecksComidas.lastChild);
+          
           }
 
  
+    add_more_fields.onclick = function(){
+      
+                var newField = document.createElement('input');
+          newField.setAttribute('type','text');
+          newField.setAttribute('name','vehicl');
+          newField.setAttribute('class','vehicl');
+          newField.setAttribute('size',50);
+          newField.setAttribute('placeholder','Another Field');
+          divChecksComidas.appendChild(newField);
+          
+          }

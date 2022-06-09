6317451919d7cc8406f6dbac7d02f169ff597a6f
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2022-06-09 16:19:47 +0000

    listo para entregar, quita los errores si valida pero se stackean si no valida

--- js_formulario.js
+++ js_formulario.js
@@ -1,39 +1,27 @@
  // 9/6/2022
 
-
+// buscar elementos <Carnes y Lomos>
 var add_more_fields = document.getElementById('add_more_fields');
 var remove_fields = document.getElementById('remove_fields');
-
-// buscar elementos <Carnes y Lomos>
 var check_carnesylomos = document.getElementById('asd');
 var controlss = document.getElementById('controls'); 
 var divChecksComidas = document.getElementById('divChecksComidas2'); 
+
+
+
+// buscar elementos que faltan - sin hacer para esta entrega, solo se usa <Carnes y Lomos>
 var divChecksComida_hamburs = document.getElementById('divChecksComidas-hambur'); 
 var check_hamburguesas = document.getElementById('asd2');
 var controls_hambur = document.getElementById('controls-hambur'); 
 var add_more_fields_hambur = document.getElementById('add_more_fields-hambur');
 var remove_fields_hambur = document.getElementById('remove_fields-hambur');
 
-
-// buscar elementos que faltan - sin hacer para esta entrega, solo se usa <Carnes y Lomos>
 var check_helado = document.getElementById('Helado');
-
-
 var check_Empanadas = document.getElementById('Empanadas');
-
-
 var check_Pizzas = document.getElementById('Pizzas');
-
-
 var check_Pasta = document.getElementById('Pasta');
-
-
 var check_Panaderia = document.getElementById('Panaderia');
-
-
 var check_Sushi = document.getElementById('Sushii');
-
-
 var check_Verduras = document.getElementById('Verdurass');
 
 
@@ -105,6 +93,9 @@ remove_fields.onclick = function(){
           console.log("holalengght checks comidas");
           console.log(numb);
           }
+
+
+
 
 
 
@@ -200,7 +191,7 @@ let elemento_div_mensaje =document.getElementById("div_mensaje");
 let elemento_nombre_negocio =document.getElementById("nombre_negocio_input");
 let elemento_mail_negocio =document.getElementById("nombre_mail_input");
 let elemento_biografia_negocio = document.getElementById("biografia_input");
-let elemento_inputs_extras = document.getElementsByClassName("para_1");
+let elemento_inputs_extras = document.getElementsByClassName("para_2");
 
 let msj_aux = "En breve nos contactaremos a su email para finalizar el ingreso a la plataforma.";
 
@@ -221,16 +212,23 @@ function enviar(event) {
   if (validar()) {
 
 console.log("validó bien");
+    
 alert("Datos bien ingresados");
+    
 div_form.style.display = "none";
 div_DescForm.style.display = "none";
+    
 f1.innerHTML = `Gracias por dejar sus datos ${elemento_nombre_negocio.value}  `;
 f2.innerHTML = `Usted se inscribió con el email: ${elemento_mail_negocio.value}  `;
 f3.innerHTML = `Y escribió la biografía: ${elemento_biografia_negocio.value}  `;
+    
 elemento_div_mensaje.append(f1);
 elemento_div_mensaje.append(f2);
 elemento_div_mensaje.append(f3);
 elemento_div_mensaje.append(msj_aux);
+    
+
+    
 }
   else{
     alert("Datos mal ingresados");
@@ -250,13 +248,10 @@ function validar() {
 console.log("validar");
   
   let list_errores = [];
-  
+  let inputs_extras_validados = false;
   let elements = document.getElementsByName("vehicl");
-
   let p_del_elmt_nbrm_ngco = document.createElement("p");
-
   let p_del_elmt__mail_ngco = document.createElement("p");
-
   let p_del_elemento_biografia_negocio = document.createElement("p");
 
   /*
@@ -275,54 +270,56 @@ console.log("validar");
   
 // TODAVÍA NO FUNCIONA BIEN, NO SÉ TODAVÍA CÓMO BORRAR CADA ERROR DE CADA INPUT 
 //--------------------------------------
-   elements.forEach((rate) => {
+  
+elements.forEach ( (rate) => {
      
       let p_del_rate = document.createElement("p");
-      p_del_rate.setAttribute("id","para_1");
-      
-      // divChecksComidas2.removeChild(divChecksComidas2.lastChild);
-      
-     if (rate.value.match(regex_solo_letras_numeros) )     {
+      p_del_rate.setAttribute("class","para_2");
+
+  
+if (rate.value.match(regex_solo_letras_numeros) )     {
+  
        console.log("super regex rate");
-       
        p_del_rate.innerHTML = " ";
-       rate.style.border = "revert";
-       document.getElementById("divChecksComidas2").append(p_del_rate);
-       //divChecksComidas.removeChild(divChecksComidas.lastChild);
-      
+       rate.style.border = "5px solid green";
+
        } 
      
-     else if (!rate.value.match(regex_solo_letras_numeros) || rate.value.trim() == ''){ 
-       
-            console.log("not super regex rate");
-       
-           
-           rate.style.border = "3px solid red";
-       
-            //divChecksComidas.removeChild(divChecksComidas.lastChild);
-            p_del_rate.innerHTML = "Por favor, ingrese un elemento válido (solo letras y números)";
-            document.getElementById("divChecksComidas2").append(p_del_rate);
-        
-            list_errores.push(1);
-       
-            }
+  else if (!rate.value.match(regex_solo_letras_numeros) || rate.value.trim() == ''){ 
+         
+              console.log("not super regex rate");
+             rate.style.border = "3px solid red";
+              //divChecksComidas.removeChild(divChecksComidas.lastChild);
+              p_del_rate.innerHTML = "Por favor, ingrese un elemento válido (solo letras y números)";
+              document.getElementById("divChecksComidas2").append(p_del_rate);
+              list_errores.push(1);
+    
+              }
      
-});
+} ) ;
 
+  // borrar mensajes de error de input de elementos extra
+  if (list_errores == 0) {
+    while(elemento_inputs_extras[0]) {
+    elemento_inputs_extras[0].parentNode.removeChild(elemento_inputs_extras[0]);
+}
+  }
 
+  
   //--------------------------------------
   // acá empieza a obtener y validar <nombre>, <email>, <biografía>
   //--------------------------------------
 
+  
   //--------------------------------------
   // validar si input de <nombre> es texto correcto, solo letras
   //--------------------------------------
      if  (elemento_nombre_negocio.value.match(regex_solo_letras) )  {
        console.log("super regex name");
-       
+       elemento_nombre_negocio.style.border = "3px solid green";
        p_del_elmt_nbrm_ngco.innerHTML = " ";
-       divNombreNegocio.removeChild(divNombreNegocio.lastChild);
-       document.getElementById("divNombreNegocio").append(p_del_elmt_nbrm_ngco);
+       //divNombreNegocio.removeChild(divNombreNegocio.lastChild);
+       //document.getElementById("para_1").remove(p_del_elmt_nbrm_ngco);
       
      } else if (!elemento_nombre_negocio.value.match(regex_solo_letras) || elemento_nombre_negocio.value.trim() == '') { 
             console.log("not super regex name");
@@ -334,12 +331,15 @@ console.log("validar");
        
             }
 
+
+  
 //--------------------------------------
 // validar si input de <email> es email correcto
 //--------------------------------------
        if (regex_solo_email.test(elemento_mail_negocio.value) )    {
        console.log("super regex email");
        p_del_elmt__mail_ngco.innerHTML = " ";
+         elemento_mail_negocio.style.border = " 5px solid green";
        divEmailNegocio.removeChild(divEmailNegocio.lastChild);
        document.getElementById("divEmailNegocio").append(p_del_elmt__mail_ngco);
       
@@ -353,7 +353,7 @@ console.log("validar");
        
             }
 
-  
+  //----- logs para chequear  cosas-------------------
       if (document.getElementById("divChecksComidas2").childElementCount > 1 ) {
         console.log("childElementCount eeeeeeeeeee")
       }
@@ -365,12 +365,15 @@ console.log("validar");
         
 
 console.log("casi super regex elemento_biografia_negocio");
+
+
   
 //--------------------------------------
 // validar si input de <biografia> es texto correcto, solo letras
 //--------------------------------------
      if (elemento_biografia_negocio.value.match(regex_solo_letras_numeros) )     {
       console.log("super regex elemento_biografia_negocio");
+       elemento_biografia_negocio.style.border = " 5px solid green";
        p_del_elemento_biografia_negocio.innerHTML = " ";
        divBiografia.removeChild(divBiografia.lastChild);
        document.getElementById("divBiografia").append(p_del_elemento_biografia_negocio);
@@ -386,9 +389,9 @@ console.log("casi super regex elemento_biografia_negocio");
             }
 
 
-
-let elementos_p__alerta_elemto_extra_erroneo = document.getElementById("para_1");
 /* 
+let elementos_p__alerta_elemto_extra_erroneo = document.getElementById("para_1");
+
 
 elementos_p__alerta_elemto_extra_erroneo.remove();
 
